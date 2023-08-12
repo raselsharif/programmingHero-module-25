8a3187d1e90fecd8f7de12086f35e59e8dca861c
raselsharif <raselsharif9@gmail.com>
2023-08-12 21:35:22 +0600

    Add onclick handler directly or via javascript

---
+++ js/event.js
@@ -0,0 +1,17 @@
+function blue(){
+    document.body.style.backgroundColor='blue';
+}
+
+const purple = document.getElementById("purple");
+purple.onclick = purpleColor;
+
+function purpleColor(){
+    document.body.style.backgroundColor='purple';
+}
+
+const yellow = document.getElementById("yellow");
+yellow.onclick = yellowColor;
+
+function yellowColor(){
+    document.body.style.backgroundColor='yellow';
+}
